40b2b2b5fce423104573a030d648c113e310e611
Tramnvph14967 <94088002+Tramnvph14967@users.noreply.github.com>
2022-01-28 11:15:18 +0700

    update

--- assignment/src/pages/detailNews.js
+++ assignment/src/pages/detailNews.js
@@ -1,17 +1,36 @@
-import data from "../data";
+// import data from "../data";
+
+// const DetailNewsPage = {
+//     print(id) {
+        
+//         const result = data.find((post) => post.id == id);
+        
+//         return `
+//         <div class="max-w-5xl mx-auto" >
+//             <h1>${result.title}</h1>
+//             <img src="${result.img}" />
+//             <div>${result.desc}</div>
+//         </div>
+//         `;
+//     },
+// };
+// export default DetailNewsPage;
+
+
+
+// import data from "../data";
+
+import axios from "axios";
+import { get } from "../api/post";
 
 const DetailNewsPage = {
-    print(id) {
-        
-        const result = data.find((post) => post.id == id);
-        
+    async render(id) {
+        const { data } = await get(id);
         return `
-        <div class="max-w-5xl mx-auto" >
-            <h1>${result.title}</h1>
-            <img src="${result.img}" />
-            <div>${result.desc}</div>
-        </div>
-        `;
+            <h1>${data.title}</h1>
+            <img src="${data.img}" />
+            <div>${data.desc}</div>
+        `
     },
 };
 export default DetailNewsPage;
